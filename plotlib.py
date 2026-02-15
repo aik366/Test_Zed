ea0ab296630ffb0619f93737bc254e9d3dec5123
aik366 <haykooo3@gmail.com>
2026-02-15 14:32:35 +0300

    update 15.02.26

--- plotlib.py
+++ plotlib.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from run import *
+
+import run
 
 # Данные
-y = [metraj_year(year=25, month=i) for i in range(1, 13)]
-x = np.arange(len(y))
+y = [run.metraj_year(year=25, month=i) for i in range(1, 13)]
+mount = ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сен", "Окт", "Ноя", "Дек"]
+x = [f"{mount[i]}\n{y[i]:.0f}" for i in range(12)]
 
 # Создание графика
 fig, ax = plt.subplots(figsize=(8, 5))
 
-ax.bar(x, y, color='#9f2b68', edgecolor='white', width=0.8, align='edge')
+ax.bar(x, y, color="#9f2b68", edgecolor="white", width=0.8, align="center")
 # Цвета
 
 # Сохранение и отображение
 plt.tight_layout()
-plt.savefig('sine_plot.png', dpi=300, bbox_inches='tight')
+plt.savefig("sine_plot.png", dpi=300, bbox_inches="tight")
 plt.show()
